11ac7902050cb6a1679c8c80ca37c280850f9d2f
Gabriel Cabral <cabral.9santos@gmail.com>
2018-09-21 14:16:21 -0300

    ajuste no tipo de campo da seed PaginasSeeds

--- database/seeds/PaginasSeeds.php
+++ database/seeds/PaginasSeeds.php
@@ -24,7 +24,7 @@ class PaginasSeeds extends Seeder
         $paginaSobre->descricao = "Descricao breve sobre a empresa.";
         $paginaSobre->texto = "Texto sobre a empresa.";
         $paginaSobre->imagem = "img/modelo_img_home.jpg";
-        $paginaSobre->mapa = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.1177241914506!2d-43.36403258503297!3d-22.98269808497323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bda2ec9620a2d%3A0x5c8412e8969fa343!2sDevMedia!5e0!3m2!1spt-BR!2sbr!4v1534873760941" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>';
+        $paginaSobre->mapa = 'Mapa do maroto';
         $paginaSobre->tipo = "sobre";
 
         $paginaSobre->save();
