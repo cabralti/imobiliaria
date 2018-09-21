286e40eefe5b22ad093950c1b2c66515a7f594fb
Gabriel Cabral <cabral.9santos@gmail.com>
2018-09-21 14:32:01 -0300

    Atualizando seed Paginas

--- database/seeds/PaginasSeeds.php
+++ database/seeds/PaginasSeeds.php
@@ -21,10 +21,10 @@ class PaginasSeeds extends Seeder
         }
 
         $paginaSobre->titulo = "A Empresa";
-        $paginaSobre->descricao = "Descricao breve sobre a empresa.";
+        $paginaSobre->descricao = "Descrição breve sobre a empresa.";
         $paginaSobre->texto = "Texto sobre a empresa.";
         $paginaSobre->imagem = "img/modelo_img_home.jpg";
-        $paginaSobre->mapa = 'Mapa do maroto';
+        $paginaSobre->mapa = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.1177241914506!2d-43.364032585032966!3d-22.98269808497323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bda2ec9620a2d%3A0x5c8412e8969fa343!2sDevMedia!5e0!3m2!1spt-BR!2sbr!4v1537550553975" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>';
         $paginaSobre->tipo = "sobre";
 
         $paginaSobre->save();
