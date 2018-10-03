ade3f05c7d3ff7e79704a5038c235b16fa7d9069
Gabriel Cabral <cabral.9santos@gmail.com>
2018-10-03 15:35:58 -0300

    Criando o modelo e migracao da galeria

--- app/Imovel.php
+++ app/Imovel.php
@@ -20,5 +20,9 @@ class Imovel extends Model
         return $this->belongsTo('App\Cidade', 'cidade_id');
     }
 
+    public function galeria()
+    {
+        return $this->hasMany('App\Galeria', 'imovel_id');
+    }
 
 }
