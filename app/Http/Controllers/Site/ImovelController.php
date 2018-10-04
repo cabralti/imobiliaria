5c88f2a0c05235f6a21666a97155ca1a0b80ebdc
Gabriel Cabral <cabral.9santos@gmail.com>
2018-10-04 10:14:04 -0300

    Site - View de detalhe do imovel

--- app/Http/Controllers/Site/ImovelController.php
+++ app/Http/Controllers/Site/ImovelController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Imovel;
 
 class ImovelController extends Controller
 {
-    public function index()
+    public function index($id)
     {
-        return view('site.imovel');
+        $imovel = Imovel::find($id);
+        $galeria = $imovel->galeria()->orderBy('ordem')->get();
+        $direcaoImagem = [
+            'center-align',
+            'left-align',
+            'right-align'
+        ];
+        return view('site.imovel', compact('imovel', 'galeria', 'direcaoImagem'));
     }
 }
--- resources/views/site/imovel.blade.php
+++ resources/views/site/imovel.blade.php
@@ -1,65 +1,63 @@
 @extends('layouts.site')
 
 @section('content')
-<div class="container">
-    <div class="row section">
-        <h3 class="center-align">Imóvel</h3>
-        <div class="divider"></div>
-    </div>
-    <div class="row section">
-        <div class="col s12 m8">
-            <div class="slider">
-                <ul class="slides">
-                    <li>
-                        <img src="{{ asset('img/modelo_detalhe_1.jpg') }}" alt="" class="responsive-img">
-                        <div class="caption center-align">
-                            <h3>Titulo da Imagem</h3>
-                            <h5>Descrição do Slide</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="{{ asset('img/modelo_detalhe_2.jpg') }}" alt="" class="responsive-img">
-                        <div class="caption center-align">
-                            <h3>Titulo da Imagem</h3>
-                            <h5>Descrição do Slide</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="{{ asset('img/modelo_detalhe_3.jpg') }}" alt="" class="responsive-img">
-                        <div class="caption center-align">
-                            <h3>Titulo da Imagem</h3>
-                            <h5>Descrição do Slide</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="{{ asset('img/modelo_detalhe_4.jpg') }}" alt="" class="responsive-img">
-                        <div class="caption center-align">
-                            <h3>Titulo da Imagem</h3>
-                            <h5>Descrição do Slide</h5>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class="row center-align">
-                <button onclick="sliderPrev()" class="btn blue">Anterior</button>
-                <button onclick="sliderNext()" class="btn blue">Próximo</button>
-            </div>
+    <div class="container">
+        <div class="row section">
+            <h3 class="center-align">Imóvel</h3>
+            <div class="divider"></div>
         </div>
-        <div class="col s12 m4">
-            <h4>Título do Imóvel</h4>
-            <blockquote>
-               Descrição breve sobre o imóvel
-            </blockquote>
-            <p><b>Código: </b> 245</p>
-            <p><b>Status: </b> Vender</p>
-            <p><b>Tipo: </b> Alvenaria</p>
-            <p><b>Endereço: </b> Centro</p>
-            <p><b>CEP: </b> 68743-657</p>
-            <p><b>Cidade: </b> Castanhal</p>
-            <p><b>Valor:</b> R$ 200.000,00</p>
+        <div class="row section">
+            <div class="col s12 m8">
+                @if($imovel->galeria()->count())
+                    <div class="slider">
+                        <ul class="slides">
+                            @foreach($galeria as $imagem)
+                                <li>
+                                    <img src="{{ asset($imagem->imagem) }}" alt="" class="responsive-img">
+                                    <div class="caption {{$direcaoImagem[rand(0,2)]}}">
+                                        <h3>{{$imagem->titulo}}</h3>
+                                        <h5>{{$imagem->descricao}}</h5>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="row center-align">
+                        <button onclick="sliderPrev()" class="btn blue">Anterior</button>
+                        <button onclick="sliderNext()" class="btn blue">Próximo</button>
+                    </div>
+                @else
+                    <img src="{{asset($imovel->imagem)}}" class="responsive-img"/>
+                @endif
+            </div>
+            <div class="col s12 m4">
+                <h4>{{ $imovel->titulo }}</h4>
+                <blockquote>
+                    {{ $imovel->descricao }}
+                </blockquote>
+                <p><b>Código: </b> {{ $imovel->id }}</p>
+                <p><b>Status: </b> {{ $imovel->status }}</p>
+                <p><b>Tipo: </b> {{ $imovel->tipo->titulo }}</p>
+                <p><b>Endereço: </b> {{ $imovel->endereco }}</p>
+                <p><b>CEP: </b> {{ $imovel->cep }}</p>
+                <p><b>Cidade: </b> {{ $imovel->cidade->nome }}</p>
+                <p><b>Valor:</b> R$ {{ number_format($imovel->valor, '2', ',', '.') }}</p>
 
-            <a href="{{ route('site.contato') }}" class="btn deep-orange darken-1">Entrar em contato</a>
+                <a href="{{ route('site.contato') }}" class="btn deep-orange darken-1">Entrar em contato</a>
+            </div>
+        </div>
+        <div class="row section">
+            <div class="col s12 m8">
+                <div class="video-container">
+                    {!! $imovel->mapa !!}
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <h4>Detalhes:</h4>
+                <p>
+                    {{ $imovel->detalhes }}
+                </p>
+            </div>
         </div>
     </div>
-</div>
 @endsection
