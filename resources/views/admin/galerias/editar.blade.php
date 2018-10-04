03396fea3b2f3358cf7f595587157d33b3eab6e5
Gabriel Cabral <cabral.9santos@gmail.com>
2018-10-04 09:15:03 -0300

    Galeria - Edicao e delecao de imagens da galeria

--- resources/views/admin/galerias/editar.blade.php
+++ resources/views/admin/galerias/editar.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container">
-    <h2 class="center">Editar Imóvel</h2>
+    <h2 class="center">Editar Imagem</h2>
 
     <div class="row">
         <nav>
@@ -11,19 +11,20 @@
                 <div class="col s12">
                     <a href="{{ route('admin.principal') }}" class="breadcrumb">Início</a>
                     <a href="{{ route('admin.imoveis') }}" class="breadcrumb">Lista de Imóveis</a>
-                    <a href="#" class="breadcrumb">Editar Imóvel</a>
+                    <a href="{{ route('admin.galerias', $imovel->id) }}" class="breadcrumb">Galeria de imagens</a>
+                    <a href="#" class="breadcrumb">Editar Imagem</a>
                 </div>
             </div>
         </nav>
     </div>
     
     <div class="row">
-        <form action="{{ route('admin.imoveis.atualizar', $registro->id) }}" method="post" enctype="multipart/form-data">
+        <form action="{{ route('admin.galerias.atualizar', $registro->id) }}" method="post" enctype="multipart/form-data">
             {{ csrf_field() }}
 
             <input type="hidden" name="_method" value="put"/>
 
-            @include('admin.imoveis._form')
+            @include('admin.galerias._form')
 
             <button class="btn blue">Atualizar</button>
         </form>
